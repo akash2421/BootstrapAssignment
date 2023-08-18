41c6e3c917ed85ffb8bb0bb6650cde8a205f17fb
Akash_suthar <Akashsuthar2015@gmail.com>
2023-08-18 13:59:39 +0530

    cards changes

--- script.js
+++ script.js
@@ -1,24 +1,10 @@
-const wrapper = document.querySelector(".wrapper");
 const carousel = document.querySelector(".carousel");
-const firstCardWidth = carousel.querySelector(".card").offsetWidth;
+const firstCard = carousel.querySelector(".card");
 const arrowBtns = document.querySelectorAll(".wrapper i");
-const carouselChildrens = [...carousel.children];
-
-
-let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
-
-carouselChildrens.slice(-cardPerView).forEach(card => {
-    carousel.insertAdjacentHTML("afterbegin", card.outerHTML);
-});
-
-
-
 
 arrowBtns.forEach(btn => {
     btn.addEventListener("click", () => {
-        carousel.scrollLeft += btn.id == "left" ? -firstCardWidth : firstCardWidth;
+        const scrollAmount = btn.id === "left" ? -firstCard.offsetWidth : firstCard.offsetWidth;
+        carousel.scrollLeft += scrollAmount;
     });
 });
-
-
-
